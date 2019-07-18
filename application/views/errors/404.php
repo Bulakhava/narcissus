577034c7e124efbcef0dd8d72c75aaebcf0177a1
olga.bulakhova <olga.bulakhova@hiendsys.com>
2019-07-18 11:53:55 +0300

    404

--- application/views/errors/404.php
+++ application/views/errors/404.php
@@ -1,1 +1,22 @@
-страница 404
+<?php include($_SERVER['DOCUMENT_ROOT'].'/application/views/layouts/head.php'); ?>
+
+<div class="container">
+
+    <?php include($_SERVER['DOCUMENT_ROOT'].'/application/views/layouts/header.php'); ?>
+
+    <main>
+        <div class="err-404">
+            <div class="err-404_img">
+                <span class="font-18">Error</span><span class="font-135">404</span>
+            </div>
+            <div class="err-404_title">Страница не найдена.</div>
+            <div class="err-404_text">Возможно, вы воспользовались недействительной ссылкой или страница была удалена.</div>
+            <div class="err-404_btn">
+                <a href="/" class="butt butt-white">На главную</a>
+            </div>
+        </div>
+    </main>
+
+    <?php include($_SERVER['DOCUMENT_ROOT'].'/application/views/layouts/footer.php'); ?>
+
+</div>
--- application/views/layouts/head.php
+++ application/views/layouts/head.php
@@ -3,7 +3,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title><?php echo $title ?></title>
+    <title><?php echo (isset($title) ? $title : '404') ?></title>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css?family=Roboto:100,200,300,400,700,800" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Montserrat:100,200,300,500,600,400,700,800" rel="stylesheet">
